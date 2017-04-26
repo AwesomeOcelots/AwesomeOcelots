a0c08a87e9a9f9cda2845a569e34648e5ce3bb00
Sebastian Boberg <sebastianboberg@gmail.com>
2017-04-26 16:16:05 -0700

    mySQL database schema

---
+++ Server/schema.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE greener;
+
+USE greener;
+
+CREATE TABLE cities (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(20) NOT NULL,
+  img_uri varchar(20),
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  home_addr varchar(100),
+  work_addr varchar(100),
+  home_city_id int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (home_city_id) REFERENCES cities(id)
+);
+
+CREATE TABLE likes (
+  id int NOT NULL AUTO_INCREMENT,
+  user_id int,
+  city_id int,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (city_id) REFERENCES cities(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < server/schema.sql
+ *  to create the database and the tables.*/
