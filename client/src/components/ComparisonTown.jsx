27f03ac757d8b4cbc5bda610f8e138c1649c9463
Simmba <simmern.kang@gmail.com>
2017-05-15 14:48:06 -0700

    can i push now? finalized css

--- client/src/components/ComparisonTown.jsx
+++ client/src/components/ComparisonTown.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import App from './App'
-import Greener from './Greener.jsx'
 import LunchThere from './LunchThere.jsx'
 import TrafficThere from './TrafficThere.jsx'
 import WeatherThere from './WeatherThere.jsx'
@@ -12,6 +11,9 @@ class ComparisonTown extends React.Component {
       <div>
         <div>
           <img id='othertown' src="https://res.cloudinary.com/simpleview/image/upload/c_fill,f_auto,h_767,q_65,w_1663/v1/clients/columbus/meetings_skyline_08ac1260-445a-47ce-9655-3039e6a8d6b7.jpg"></img>
+        <p id="cweatherd"> sdfgfdsgsdg </p>
+        <p id="ctrafficd"> sdfgdsgdfg </p>
+        <p id="clunchd"> sdfgdsfgdsfgdfsgdsfgdfsg </p>
         </div>
         <div id="footer"></div>
       </div>
