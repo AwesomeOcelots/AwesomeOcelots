0dfa3e425dae83c0ec03f78517971bf1797ef33a
Sebastian Boberg <sebastianboberg@gmail.com>
2017-05-15 15:23:06 -0700

    refactor weather to use callbacks

--- server/middleware/weather.js
+++ server/middleware/weather.js
@@ -3,15 +3,19 @@ var DarkSky = require('forecast.io');
 //API key
 var key = '1028df8f37a758c1f061c7f1fcf81952';
 //options object needed for the wrapper library to work
-module.exports= (res) => {
+module.exports= (address, cb) => {
   var options = {
     APIKey: key,
     timeout: 1000
   },
   darksky = new DarkSky(options);
-  darksky.getAtTime(37.789935,-122.398961, Date.now, function (err, response, data) {
-  if (err) throw err;
-  data = JSON.parse(JSON.stringify(data.currently));
-  res.send(data);
-  })
+  // TODO: convert address to coordinates
+  darksky.getAtTime(latitude, longitude, Date.now, function (err, response, data) {
+    if (err) {
+      cb(err, null);
+    } else {
+      data = JSON.parse(JSON.stringify(data.currently));
+      cb(null, data);
+    }
+  });
 }
